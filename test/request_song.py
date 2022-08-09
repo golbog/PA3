db117fa5a436fa7cc39a337f11cfbafe1903466f
golbog <bogdan.golobic@gmail.com>
2022-08-09 10:56:23 +0200

    Delete test directory

--- test/request_song.py
+++
@@ -1,9 +0,0 @@
-import requests
-d = {
-  "song": "tipi",
-  "parts": ["bobni_rok.mp3", "synth.mp3"]
-}
-r = requests.post("http://127.0.0.1:7880/song/song_mix", json=d)
-print(r)
-with open("test.mp3", "wb") as f:
-    f.write(r.content)
